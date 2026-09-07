331cc8081ba44211432ed23e425c8bbef320b818
faculdadecorporativa <faculdadecorporativamb@gmail.com>
2026-09-07 18:33:33 -0400

    fix: load PocketBase via CDN

--- pb.js
+++ pb.js
@@ -1,4 +1,5 @@
-import PocketBase from 'pocketbase';
+import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.21.3/+esm';
+
 export const pb = new PocketBase('https://pb.faculdadecorporativa.com.br');
 pb.autoCancellation(false);
 window.pb = pb;
